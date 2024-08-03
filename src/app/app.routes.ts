7364f953822d99c53f0fedfa5753eaf86a6b0f07
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 18:41:27 +1200

    Added link to reviews page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {PlanetComponent} from "./pages/planet/planet.component";
 import {WeatherWidgetComponent} from "./components/weather-widget/weather-widget.component";
+import {ReviewsComponent} from "./pages/planet/reviews/reviews.component";
 
 export const routes: Routes = [
   {
@@ -16,6 +17,10 @@ export const routes: Routes = [
         path: '',
         pathMatch: 'full',
         component: WeatherWidgetComponent
+      },
+      {
+        path: 'reviews',
+        component: ReviewsComponent
       }
     ]
   }
