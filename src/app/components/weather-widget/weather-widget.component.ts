bbca2ac5a60bd800be5217782d0219b10fccf367
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 13:47:44 +1200

    created weather widget

---
+++ src/app/components/weather-widget/weather-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-widget.component.html',
+  styleUrl: './weather-widget.component.scss'
+})
+export class WeatherWidgetComponent {
+
+}
