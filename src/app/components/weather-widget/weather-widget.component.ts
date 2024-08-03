6955601a44874b7a0037ca08617da2ccf31d2a67
rxia357 <100566786+rxia357@users.noreply.github.com>
2024-08-03 23:12:01 +1200

    lmao

--- src/app/components/weather-widget/weather-widget.component.ts
+++ src/app/components/weather-widget/weather-widget.component.ts
@@ -9,35 +9,35 @@ import {PlanetsService} from "../../services/planets.service";
   templateUrl: './weather-widget.component.html',
   styleUrls: ['./weather-widget.component.scss'],
 })
-export class WeatherWidgetComponent implements OnInit {
-  planetGuid = input.required<string>();
-  planetService = inject(PlanetsService);
-  planet = computed(() => {
-    return this.planetService.planets().find(planet => planet.guid === this.planetGuid());
-  });
-
-  weatherData: any;
-  lat: number = 36.8509; // Default latitude (e.g., Auckland)
-  lon: number = 174.7645; // Default longitude (e.g., Auckland)
-  apiKey = '919c34567d733393ab8a9e3cbcd39f1e'; // API key
-  apiUrl = 'https://api.openweathermap.org/data/2.5/onecall';
-
-  ngOnInit(): void {
-    this.getWeather();
-  }
-
-  getWeather(): void {
-    fetch(
-      `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&exclude=minutely,hourly&appid=${this.apiKey}&units=metric`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.weatherData = data;
-      })
-      .catch((error) => {
-        console.error('Error fetching weather data', error);
-      });
-  }
+export class WeatherWidgetComponent {
+  // planetGuid = input.required<string>();
+  // planetService = inject(PlanetsService);
+  // planet = computed(() => {
+  //   return this.planetService.planets().find(planet => planet.guid === this.planetGuid());
+  // });
+  //
+  // weatherData: any;
+  // lat: number = 36.8509; // Default latitude (e.g., Auckland)
+  // lon: number = 174.7645; // Default longitude (e.g., Auckland)
+  // apiKey = '919c34567d733393ab8a9e3cbcd39f1e'; // API key
+  // apiUrl = 'https://api.openweathermap.org/data/2.5/onecall';
+  //
+  // ngOnInit(): void {
+  //   this.getWeather();
+  // }
+  //
+  // getWeather(): void {
+  //   fetch(
+  //     `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&exclude=minutely,hourly&appid=${this.apiKey}&units=metric`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       this.weatherData = data;
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching weather data', error);
+  //     });
+  // }
 
   getDayOfWeek(index: number): string {
     const days = [
