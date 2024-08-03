553e440da0758f535ae5eea4ee30c7902a63da43
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 23:33:10 +1200

    added anmimated home page

---
+++ src/app/directives/random-move.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RandomMoveDirective } from './random-move.directive';
+
+describe('RandomMoveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RandomMoveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {PlanetsService} from "../../services/planets.service";
 import {RouterLink} from "@angular/router";
+import {RandomMoveDirective} from "../../directives/random-move.directive";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
+    RandomMoveDirective,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
