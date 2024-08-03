8440c4dc2e357c905af8edf244f25fe4636160df
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 15:57:41 +1200

    rebase?

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-<<<<<<< HEAD
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
     provideAnimationsAsync()
   ]
-=======
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
->>>>>>> 23813cc6035375b852019bd195d97a20bc0d0ab6
 };
