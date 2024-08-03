ee04a4443dc56169a08e6b889d162fd292864965
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 15:01:53 +1200

    Updated home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {PlanetsService} from "../../services/planets.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  planetsService = inject(PlanetsService);
 }
