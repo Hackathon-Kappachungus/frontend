ded21a21deba2dc7e573c7d4340829d42a589c62
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 17:38:37 +1200

    added better opening and more data data is now filtered by catagory

--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -17,14 +17,82 @@ export class LanguageComponent {
   planet = input.required();
   phrases = [
     {
-      phrase: 'Test tone 1',
+      catagory: 'greetings',
+      text: 'Test tone 1',
       url: '/assets/language-sounds/test_tone.mp3',
     },
     {
-      phrase: 'Test tone 2',
+      catagory: 'greetings',
+      text: 'Test tone 2',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'greetings',
+      text: 'Test tone 3',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'greetings',
+      text: 'Test tone 4',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'greetings',
+      text: 'Test tone 5',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'navigation',
+      text: 'RANDOMFDS',
+      url: '/assets/language-sounds/test_tone.mp3',
+    },
+    {
+      catagory: 'navigation',
+      text: 'SFDJLKFDSL',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'navigation',
+      text: 'dsjfkdsof',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'navigation',
+      text: 'dsjflkdsn',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'navigation',
+      text: 'fdslfdslls',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'shopping',
+      text: 'fdsfklmsd',
+      url: '/assets/language-sounds/test_tone.mp3',
+    },
+    {
+      catagory: 'shopping',
+      text: 'dskfnsdjl',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'shopping',
+      text: 'dsfnsdl',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'shopping',
+      text: 'dsfjkldsfj',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
+    {
+      catagory: 'shopping',
+      text: 'pdopfjsdonfdslfdls',
       url: '/assets/language-sounds/test_tone2.mp3',
     },
   ];
 
   readonly panelOpenState = signal(false);
+  panelOpened = signal<number | undefined>(undefined);
 }
