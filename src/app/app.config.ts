bd705daa2d7e3a0f4c128a3df16f8f6e7758c606
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 15:41:43 +1200

    added angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ]
 };
