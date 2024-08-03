648a0876fd988154cea9e307761ee496f7ff175c
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 15:07:10 +1200

    working with multiple audio files

--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -5,14 +5,18 @@ import { Component, input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './language.component.html',
-  styleUrl: './language.component.scss'
+  styleUrl: './language.component.scss',
 })
 export class LanguageComponent {
   planet = input.required();
   phrases = [
     {
-      phrase: 'Hello',
-      url: '/assets/language-sounds/test_tone.mp3'
-    }
+      phrase: 'Test tone 1',
+      url: '/assets/language-sounds/test_tone.mp3',
+    },
+    {
+      phrase: 'Test tone 2',
+      url: '/assets/language-sounds/test_tone2.mp3',
+    },
   ];
 }
