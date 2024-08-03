e256a62c0e8b999da04473bf10eba8c0cfcf17fb
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 14:13:57 +1200

    created language component

---
+++ src/app/components/language/language.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language',
+  standalone: true,
+  imports: [],
+  templateUrl: './language.component.html',
+  styleUrl: './language.component.scss'
+})
+export class LanguageComponent {
+
+}
