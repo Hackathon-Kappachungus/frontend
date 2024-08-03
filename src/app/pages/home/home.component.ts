18a26d7b28157186aaddd390da262d76bdfeb9f7
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 14:59:31 +1200

    test tone working

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LanguageComponent } from "../../components/language/language.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LanguageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
