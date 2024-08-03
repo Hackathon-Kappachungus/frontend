236f51e7ea9ec93e3bdde0d5e8474b8409f1e963
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 16:38:37 +1200

    added expantion panel

--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -1,12 +1,17 @@
-import { Component, input } from '@angular/core';
+
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ChangeDetectionStrategy, Component, signal,input } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-language',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatExpansionModule, MatIconModule, MatInputModule],
   templateUrl: './language.component.html',
   styleUrl: './language.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LanguageComponent {
   planet = input.required();
@@ -20,4 +25,6 @@ export class LanguageComponent {
       url: '/assets/language-sounds/test_tone2.mp3',
     },
   ];
+
+  readonly panelOpenState = signal(false);
 }
