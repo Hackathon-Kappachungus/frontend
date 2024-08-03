65c6d28ef0ab983fbcb9804a3d3b80e33e407002
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 13:24:48 +1200

    Added page router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {PlanetComponent} from "./pages/planet/planet.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'planet',
+    component: PlanetComponent
+  }
+];
