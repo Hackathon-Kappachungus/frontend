6803e8166cdfb1cf0ae4609abed7a9c70c659244
rxia357 <100566786+rxia357@users.noreply.github.com>
2024-08-03 21:57:09 +1200

    added image links

--- src/app/services/planets.ts
+++ src/app/services/planets.ts
@@ -7,7 +7,7 @@ export const planets: planet[] = [
     imageUrl: "https://media.discordapp.net/attachments/563283988265959433/1269124473790136392/mars-homepage.png?ex=66aeebc5&is=66ad9a45&hm=38442deb3ee4b9e2548436d5cc0d9a7f94e7ed1808bf701ab62dd2cbda2bdd91&=&format=webp&quality=lossless&width=1224&height=816",
     description: "Mars, the fourth planet from the Sun, is known as the \"Red Planet\" due to its reddish appearance and is a key focus of study for its potential to harbor past or present life.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269154956846563421/mars-info-img.png?ex=66af0829&is=66adb6a9&hm=ed7e1596d3d3828521f3d759e79af44b0bdd065a09ac9a559211c11dc2a1f0b9&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269154956846563421/mars-info-img.png?ex=66af0829&is=66adb6a9&hm=ed7e1596d3d3828521f3d759e79af44b0bdd065a09ac9a559211c11dc2a1f0b9&",
     temperature: "24°C",
     current_weather: "Sunny",
     big_warning: "SEVERE DUST STORMS",
@@ -25,7 +25,7 @@ export const planets: planet[] = [
     imageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269126181014474772/jup-removebg-preview.png?ex=66aeed5c&is=66ad9bdc&hm=8d0d34d1ac737ffbbcceba015660b8090fd5eb56b901eae067c1a0add7c3d6af&",
     description: "Jupiter, the largest planet in our solar system, is a gas giant known for its Great Red Spot, strong magnetic field, and numerous moons.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269161717464698972/image.png?ex=66af0e75&is=66adbcf5&hm=7fec4ad426820b3779950f65bd4763b95e76fe58008372b0b2cf5cd99c64e999&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269161717464698972/image.png?ex=66af0e75&is=66adbcf5&hm=7fec4ad426820b3779950f65bd4763b95e76fe58008372b0b2cf5cd99c64e999&",
     temperature: "-10°C",
     current_weather: "Stormy",
     big_warning: "THUNDERSTORMS",
@@ -42,7 +42,7 @@ export const planets: planet[] = [
     imageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269126180758491146/sat-removebg-preview.png?ex=66aeed5c&is=66ad9bdc&hm=45c81bf3634ee844aac7019cb13b38a677385c126af874ec7da6844501b560a4&",
     description: "Saturn is the sixth planet from the Sun, famous for its spectacular rings, and is a gas giant primarily composed of hydrogen and helium with over 80 moons, including Titan, its largest moon.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269161554830430248/image.png?ex=66af0e4e&is=66adbcce&hm=d4c36699900fdf5b5248734ceaa4421ba6489f41e7f5f2c655281d9c1d73acd2&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269161554830430248/image.png?ex=66af0e4e&is=66adbcce&hm=d4c36699900fdf5b5248734ceaa4421ba6489f41e7f5f2c655281d9c1d73acd2&",
     temperature: "-100°C",
     current_weather: "",
     big_warning: "Freezing",
@@ -59,7 +59,7 @@ export const planets: planet[] = [
     imageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269126181299683408/ura-removebg-preview.png?ex=66aeed5c&is=66ad9bdc&hm=49a96db932bbfc5b11a6aab047f9184bee916f97ffe8e4447b1cf5db79d4ec73&",
     description: "Uranus is the seventh planet from the Sun, known for its unique sideways rotation, icy composition, faint rings, and 27 moons, including Titania and Oberon.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269202709735800843/iqi.png?ex=66af34a2&is=66ade322&hm=378a117d18b5008c8eb4f43845f5f3e1ad741e3108d0bb17105dd4c5ed54ff9f&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269202709735800843/iqi.png?ex=66af34a2&is=66ade322&hm=378a117d18b5008c8eb4f43845f5f3e1ad741e3108d0bb17105dd4c5ed54ff9f&",
     temperature: "-10°C",
     current_weather: "Stormy",
     big_warning: "THUNDERSTORMS",
@@ -76,7 +76,7 @@ export const planets: planet[] = [
     imageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269126181555277824/nep-removebg-preview.png?ex=66aeed5c&is=66ad9bdc&hm=8449bd903aba44fcf7bd1f7eb0b660cb5720e6a66af0dda75d66365bd2cc834b&",
     description: "Neptune is the eighth and farthest planet from the Sun, known for its striking blue color, strong winds, dynamic storms, and 14 moons, with Triton being the largest.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269200539330023497/nept.png?ex=66af329d&is=66ade11d&hm=3f94f2cbba082420220ae2c0a96e299a3955db6ce0733b8129fd18a3772d3bf1&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269200539330023497/nept.png?ex=66af329d&is=66ade11d&hm=3f94f2cbba082420220ae2c0a96e299a3955db6ce0733b8129fd18a3772d3bf1&",
     temperature: "-10°C",
     current_weather: "Stormy",
     big_warning: "THUNDERSTORMS",
@@ -93,7 +93,7 @@ export const planets: planet[] = [
     imageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269126181811126272/plut-removebg-preview.png?ex=66aeed5c&is=66ad9bdc&hm=f9662c06af0c8ea7264cd788e9a55c1399f38ef02c4d815bdd42f4a16f0292e0&",
     description: "Pluto is a dwarf planet in the Kuiper Belt, known for its icy surface, eccentric orbit, and five moons, with Charon being the largest.",
     warnings: ['warning1', 'warning2', 'warning3'],
-    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269199431564263495/plutooo-removebg-preview.png?ex=66af3195&is=66ade015&hm=239ae580cd3b3e11da950d9c96193afa2438e800fcca0d64a0b440202dba855a&"
+    detailImageUrl: "https://cdn.discordapp.com/attachments/563283988265959433/1269199431564263495/plutooo-removebg-preview.png?ex=66af3195&is=66ade015&hm=239ae580cd3b3e11da950d9c96193afa2438e800fcca0d64a0b440202dba855a&",
     temperature: "-10°C",
     current_weather: "Stormy",
     big_warning: "THUNDERSTORMS",
