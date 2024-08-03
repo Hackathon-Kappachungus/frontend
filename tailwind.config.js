e8806b15b6112c89d7dc6c130de45f7923bcad8a
rxia357 <100566786+rxia357@users.noreply.github.com>
2024-08-03 18:26:32 +1200

    deleted duplicate code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,6 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", // Adjust if necessary
-  ],
-  theme: {
     extend: {
       fontFamily: {
         outfit: ['Outfit', 'sans-serif'],
