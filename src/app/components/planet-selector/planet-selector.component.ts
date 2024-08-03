e1d0f1329039f1d112728ea8bd0c28458717ca7f
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2024-08-03 20:45:03 +1200

    added planet selector

---
+++ src/app/components/planet-selector/planet-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './planet-selector.component.html',
+  styleUrl: './planet-selector.component.scss'
+})
+export class PlanetSelectorComponent {
+
+}
