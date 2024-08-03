275b2b3386e358292b1095ff9261d998c193d18e
JoshuaMorley <joshua.morley31@gmail.com>
2024-08-03 13:23:37 +1200

    Created two pages

---
+++ src/app/pages/planet/planet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planet',
+  standalone: true,
+  imports: [],
+  templateUrl: './planet.component.html',
+  styleUrl: './planet.component.scss'
+})
+export class PlanetComponent {
+
+}
